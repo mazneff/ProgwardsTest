8c643f5645f58f71385d2e36fc63a05e10fbbdd4
mazneff <mazneff@yandex.ru>
2019-06-08 10:36:24 +0300

    Create Task1.java

---
+++ src/ru/progwards/java1/lessons/helloworld/Task1.java
@@ -0,0 +1,14 @@
+package ru.progwards.java1.lessons.helloworld;
+
+public class Task1 {
+	public static void main(String[] args) {
+		String s1 = "Хорошо идут дела";
+		String s2 = "Изучаю Java я!";
+		String s3 = " ";
+		
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s1+s3+s2);
+		System.out.println(s2+s3+s1);
+	}
+}
